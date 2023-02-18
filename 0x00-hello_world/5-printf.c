555cb8fef8e6ed7fd94c7608adfe911f2c5f90d7
Emmanuel Amoatey <eamoatey02@gmail.com>
2023-02-18 15:25:32 -0800

    output using printf function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- * main - this allows to code
+ * main - helps in execution
  *
- *Return: this returns an integer
+ * Return: returns 0 when successful
  */
 
 int main(void)
 {
-printf("with proper grammer, but the outcome is a piece of art\n,");
-return (0);
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
 }
