39480c750609efa4f315f46b768140676bf481e9
Emmanuel Amoatey <eamoatey02@gmail.com>
2023-02-18 12:22:48 -0800

    Hello,puts task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - allows us to write codes
+ *
+ *Return: this returns an integer
+ */
+
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle");
+return (0);
+
+}
