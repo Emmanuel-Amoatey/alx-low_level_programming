201ce2fd00dfdfc7e1cbec0b34b916045395cf02
Emmanuel Amoatey <eamoatey02@gmail.com>
2023-02-18 14:00:58 -0800

    printf task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - this allows to code
+ *
+ *Return: this returns an integer
+ */
+
+int main(void)
+{
+printf("with proper grammer, but the outcome is a piece of art,\n");
+return (0);
+}
