dd06b3ff34fc0074f76e041e4a000b67ced70c58
Emmanuel Amoatey <eamoatey02@gmail.com>
2023-02-20 03:12:51 -0800

    Outputting the last digitof a number

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,25 +2,28 @@
 #include <time.h>
 /* more headers goes there */
 #include <stdio.h>
+
 /* betty style doc for function main goes there */
 /**
  * main - the main function
  *
- * Return: returns 0 on success
+ * Return: returns 0 when success
  */
 int main(void)
 {
 	int n;
+	int a;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if (n > 0)
-		printf("%d is positive", n);
-	else if (n == 0)
-		printf("%d is zero", n);
-	else
-		printf("%d is negative", n);
+	a = n % 10;
+	if (a > 5)
+		printf("Last digit of %d is %d and is greater than 5", n, a);
+	else if (a == 0)
+		printf("Last digit of %d is %d and is 0", n, a);
+	else if (a < 6 && a != 0)
+		printf("Last digit of %d is %d and is less than 6 aand not 0", n, a);
 	printf("\n");
 	return (0);
 }
