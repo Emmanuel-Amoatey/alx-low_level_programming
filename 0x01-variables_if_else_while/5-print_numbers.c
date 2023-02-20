2585c78b5723786635eb3f1074bc77f7be8943ec
Emmanuel Amoatey <eamoatey02@gmail.com>
2023-02-20 11:52:54 -0800

    outputing numbers of base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: this is the return statement
+ */
+
+int main(void)
+{
+	char k;
+
+	for (k = 0 ; k < 10 ; k++)
+		printf("%d", k);
+	printf("\n");
+	return (0);
+}
