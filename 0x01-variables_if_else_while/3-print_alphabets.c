2253a8f5ce8de4d8ab520f10a601ee8367ed0fea
Emmanuel Amoatey <eamoatey02@gmail.com>
2023-02-20 10:10:41 -0800

    Outputting alphabeth in lowercase and then caps

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: returns an integer if code successful
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+
+	for (i = 'I' ; i <= 'Z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
