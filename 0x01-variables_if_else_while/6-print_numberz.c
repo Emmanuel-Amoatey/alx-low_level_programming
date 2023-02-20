2234dbdeb84f94f292c3fa07481c45eb571b2def
Emmanuel Amoatey <eamoatey02@gmail.com>
2023-02-20 13:49:05 -0800

    Printing numbers with int and putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - ths is the main function
+ *
+ * Return: this the return statement
+ */
+
+int main(void)
+{
+	int k;
+
+	for (k = 0 ; k < 10 ; k++)
+		putchar(k + '0');
+	putchar('\n');
+	return (0);
+}
