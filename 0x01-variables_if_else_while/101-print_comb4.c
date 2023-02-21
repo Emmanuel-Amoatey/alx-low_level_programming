a53778198e2f7b9e8d6f4e707b3155bc9342e4f7
Emmanuel Amoatey <eamoatey02@gmail.com>
2023-02-20 17:27:07 -0800

    Printing different combination of three digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ *
+ * Return: the return statement
+ */
+
+int main(void)
+{
+	int k;
+	int l;
+	int m;
+
+	for (k = 0 ; k < 10 ; k++)
+	{
+		for (l = 1 ; l < 10 ; l++)
+		{
+			for (m = 2 ; m < 10 ; m++)
+			{
+				if (k < l && l < m)
+				{
+					putchar(k + '0');
+					putchar(l + '0');
+					putchar(m + '0');
+					if (k + l + m != 24)
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
