243e5ca0e22a9746aaab2e0bf94f5fbbe91e502f
Emmanuel Amoatey <eamoatey02@gmail.com>
2023-02-22 16:57:09 -0800

    C functions and nested loops task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - a function that adds two numbers
+ *
+ * @b: the first number
+ *
+ * @d: the second number
+ *
+ * Return: will return the answer
+ */
+
+int add(int b, int d)
+{
+	return (b + d);
+}
