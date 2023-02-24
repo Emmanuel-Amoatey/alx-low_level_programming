4c6d212fbd1b0c9d5ccfd3297bfc5f5d85c8f609
Emmanuel Amoatey <eamoatey02@gmail.com>
2023-02-24 07:04:59 -0800

    Debugging task 2: calculating the largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -11,19 +11,19 @@
 int largest_number(int a, int b, int c)
 {
 	int largest;
-    
-    if (a > b && b < c)
-    {
+
+	if (a >= b && b <= c)
+	{
 	largest = a;
-    }
-    else if (b > a && a > c)
-    {
+	}
+	else if (b >= a && a <= c)
+	{
 	largest = b;
-    }
-    else
-    {
+	}
+	else
+	{
 	largest = c;
-    }
+	}
 
 	return (largest);
 }
