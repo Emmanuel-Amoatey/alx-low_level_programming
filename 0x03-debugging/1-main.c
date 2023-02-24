0669beee578e1e7cfdf9a9276d0dc7b63f0de7f9
Emmanuel Amoatey <eamoatey02@gmail.com>
2023-02-23 19:38:56 -0800

    Debugging task 1 : Like, Comment nd Subscribe

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+* main - causes an infinite loop
+* Return: 0
+*/
+
+int main(void)
+{
+        int i;
+
+        printf("Infinite loop incoming :(\n");
+
+        i = 0;
+	/* (i = 0) will make the loop go infinite because (i) remains less than 10 */
+
+        while (i < 10)
+        {
+                putchar(i);
+        }
+
+        printf("Infinite loop avoided! \\o/\n");
+
+        return (0);
+}
