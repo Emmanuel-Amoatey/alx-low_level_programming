d60d4cf68ec103e5bcb16daba5ea1603f1bdf1a6
Emmanuel Amoatey <eamoatey02@gmail.com>
2023-02-28 16:18:09 -0800

    learning pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - our main function of the code
+ *
+ * @n: pointer parameter
+ *
+ * Return:returns a number on code sucss
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+ Understanding pointers, arrays and Strings
